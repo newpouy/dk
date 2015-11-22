1819c2c1fee3a518059e48d4e2db2d007baf0cb0
dowon0511 <dowon0511@naver.com>
2015-11-22 15:19:50 +0900

    한글변환

--- dk_board/src/main/java/com/dk/board/HomeController.java
+++ dk_board/src/main/java/com/dk/board/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {11111111}.", locale);
+		logger.info("Welcome home! The client locale is {11111111111}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -32,6 +32,8 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		logger.info(""+formattedDate );
 		
 		return "home";
 	}
